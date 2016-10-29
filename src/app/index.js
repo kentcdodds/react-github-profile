6e18e537a082c206ca7ce51fb0917fcc120977bc
Kent C. Dodds <kent@doddsfamily.us>
2016-10-28 22:19:07 -0600

    simplify the start

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './index.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+function App() {
+  return <div>Hello World!</div>
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
