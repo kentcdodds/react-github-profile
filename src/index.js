623716e9baf2d9df7857b1b9ba08777b71b9f00a
Kent C. Dodds <kent@doddsfamily.us>
2018-11-17 19:16:34 -0700

    add error boundary

--- src/index.js
+++ src/index.js
@@ -2,19 +2,32 @@ import './global-styles.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router} from '@reach/router'
+import ErrorBoundary from 'react-error-boundary'
 import ThemeProvider from './shared/theme-provider'
+import {IsolatedContainer} from './shared/pattern'
 import Home from './screens/home'
 import User from './screens/user'
 import * as GitHubContext from './github-client'
+
+function ErrorFallback({error}) {
+  return (
+    <IsolatedContainer>
+      <p>There was an error</p>
+      <pre style={{maxWidth: 700}}>{JSON.stringify(error, null, 2)}</pre>
+    </IsolatedContainer>
+  )
+}
 
 function App() {
   return (
     <ThemeProvider>
       <GitHubContext.Provider>
-        <Router>
-          <Home path="/" />
-          <User path="/:username" />
-        </Router>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Router>
+            <Home path="/" />
+            <User path="/:username" />
+          </Router>
+        </ErrorBoundary>
       </GitHubContext.Provider>
     </ThemeProvider>
   )
