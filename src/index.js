0ec23f036678e81863cef14c3bbc9384b4278240
Kent C. Dodds <kent@doddsfamily.us>
2018-11-19 22:13:25 -0700

    add code-splitting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Router} from '@reach/router'
 import ErrorBoundary from 'react-error-boundary'
+import loadable from 'react-loadable'
 import ThemeProvider from './shared/theme-provider'
 import {IsolatedContainer} from './shared/pattern'
-import Home from './screens/home'
-import User from './screens/user'
 import * as GitHubContext from './github-client'
+
+function Loading(props) {
+  if (props.error) {
+    return (
+      <div>
+        Error! <button onClick={props.retry}>Retry</button>
+      </div>
+    )
+  } else if (props.pastDelay) {
+    return <div>Loading...</div>
+  } else {
+    return null
+  }
+}
+
+const Home = loadable({
+  loader: () => import('./screens/home'),
+  loading: Loading,
+  delay: 300,
+})
+
+const User = loadable({
+  loader: () => import('./screens/user'),
+  loading: Loading,
+  delay: 300,
+})
 
 function ErrorFallback({error}) {
   return (
